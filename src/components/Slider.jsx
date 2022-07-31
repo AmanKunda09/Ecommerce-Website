24fe16d03eafef61e7414eac94448d7f20a32579
Aman Kunda <amankunda9@gmail.com>
2022-07-31 18:46:06 +0530

    slider updated with props

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 import styled from 'styled-components';
+import { sliderItems } from '../data';
 
 
-const Container=styled.div`
-width:100%;
-height:100vh;
-display: flex;
-
-position: relative;`;
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
 
 const Arrow=styled.div`
 width: 50px;
@@ -27,16 +29,20 @@ right: ${props=>props.direction === "right" && "10px"};
 margin:auto;
 cursor: pointer;
 opacity: 0.5;
+z-index: 2;
 `;
 
 const Wrapper=styled.div`
 height: 100%;
+display: flex;
+transform: translate(${prop=>prop.slideIndex * -100}vw);
 `;
 const Slide=styled.div`
 width: 100vw;
 height: 100vh;
 display: flex;
 align-items: center;
+background-color: #${props=>props.bg};
 `;
 const ImgContainer=styled.div`
 height: 100%;
@@ -51,35 +57,63 @@ flex: 1;
 padding: 50px;
 `;
 
-const Title = styled.h1``;
-const Desc = styled.p``;
-const Button = styled.button``;
+const Title = styled.h1`
+font-size: 70px;
+`;
+const Desc = styled.p`
+margin: 50px 0px;
+font-size: 20px;
+font-weight: 500;
+letter-spacing: 3px;
+`; 
+const Button = styled.button`
+padding: 10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+`;
 
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+const handleClick = (direction) =>{
+  if(direction==="left")
+  {
+    setSlideIndex(slideIndex >0 ? slideIndex-1 : 2)
+  } else {
+    setSlideIndex(slideIndex< 2 ? slideIndex+1 : 0)
+  }
+  
+};
+
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={()=>handleClick("left")}>
             <ArrowLeftOutlinedIcon/>
         </Arrow>
-        <Wrapper>
-          <Slide>
+        <Wrapper slideIndex={slideIndex}>
+          {sliderItems.map(item=> (
+
+          
+          <Slide bg={item.bg}>
           <ImgContainer>
-          <Image src="https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/11/Young-woman-shopping-in-a-vintage-clothing-store-1296x728-header-1296x728.jpg?w=1155&h=1528"/>
+          <Image src={item.img}/>
           </ImgContainer>
           
           <InfoContainer>
-            <Title>Summer SALE</Title>
-            <Desc>Dont Compromise on Style!Flat 30% off for new users</Desc>
-            <Button>Show Now</Button>
+            <Title>{item.title}</Title>
+            <Desc>{item.desc}</Desc>
+            <Button>SHOW NOW</Button>
           </InfoContainer>
           </Slide>
+          ))}
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={()=>handleClick("right")}>
             <ArrowRightOutlinedIcon/>
         </Arrow>
     </Container>
-  )
-}
+  );
+};
 
 export default Slider
